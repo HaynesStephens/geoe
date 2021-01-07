2dc8d662ce575155fc69a3dd41eaf4165e802e4e
HaynesStephens <hfstephens13@gmail.com>
2021-01-07 12:56:16 -0800

    Setting up PyCharm project

--- compileFiles.py
+++ compileFiles.py
@@ -37,5 +37,5 @@ def compileControl():
         da.to_netcdf('{0}/{1}{2}.PRECT.{3}-{4}.nc'.format(savedir, prefix, n_member, start, end))
     return True
 
-compileFeedback(2)
+compileFeedback(3)
 # compileControl()
