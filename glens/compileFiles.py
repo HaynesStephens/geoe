90af201dd72706816e723b60b11e18ea5ab9a5ad
HaynesStephens <hfstephens13@gmail.com>
2021-01-08 11:21:41 -0800

    Setting up PyCharm project

--- glens/compileFiles.py
+++ glens/compileFiles.py
@@ -4,27 +4,26 @@ from glob import glob
 import matplotlib
 import matplotlib.pyplot as plt
 
-def compileFeedback(n):
+def compileFeedback():
     prefix = 'b.e15.B5505C5WCCML45BGCR.f09_g16.feedback.'
     filedir = '/glade/u/home/hayness/glens/feedback'
-    n_member = str(n).zfill(3)
-    print(n_member)
-    print('LOADING FILES')
-    filenames = glob('{0}/{1}{2}*.nc'.format(filedir, prefix, n_member))
-    filenames.sort()
-    start = filenames[0].split('-')[0].split('.')[-1]
-    end   = filenames[-1].split('-')[-1].split('.')[0]
-    print('LOADING ARRAY')
-    da = xr.concat([xr.open_dataset(name)['PRECT'] for name in filenames], dim='time')
-    savedir = '/glade/u/home/hayness/glens/custom'
-    print('SAVING ARRAY')
-    savename = '{0}/{1}{2}.PRECT.{3}-{4}.nc'.format(savedir, prefix, n_member, start, end)
-    print(savename)
-    # da.to_netcdf(savename)
-    # print('ARRAY SAVED\n')
+    ensemble_members = np.arange(1, 21)
+    for n_member in ensemble_members:
+        n_member = str(n_member).zfill(3)
+        print(n_member)
+        print('LOADING FILES')
+        filenames = glob('{0}/{1}{2}*.nc'.format(filedir, prefix, n_member))
+        filenames.sort()
+        ds = [xr.open_dataset(name)['PRECT'] for name in filenames]
+        savedir = '/glade/u/home/hayness/glens/custom/prect'
+        print('SAVING ARRAYS')
+        [da.to_netcdf(name) for da,name in zip(ds, filenames)]
+        print(savename)
+        # da.to_netcdf(savename)
+        # print('ARRAY SAVED\n')
     return da, savename
 
-def compileControl():
+def compileRCP():
     prefix = 'b.e15.B5505C5WCCML45BGCR.f09_g16.control.'
     filedir = '/glade/collections/glens/Control/atm/proc/tseries/daily/PRECT'
     ensemble_members = np.arange(1,21)
@@ -39,6 +38,21 @@ def compileControl():
         da.to_netcdf('{0}/{1}{2}.PRECT.{3}-{4}.nc'.format(savedir, prefix, n_member, start, end))
     return True
 
-da, savename = compileFeedback(2)
+def compileControl():
+    prefix = 'b.e15.B5505C5WCCML45BGCR.f09_g16.control.'
+    filedir = '/glade/collections/glens/Control/atm/proc/tseries/daily/PRECT'
+    ensemble_members = np.arange(4,21)
+    for n_member in ensemble_members:
+        n_member = str(n_member).zfill(3)
+        filenames = glob('{0}/{1}{2}*.nc'.format(filedir, prefix, n_member))
+        filenames.sort()
+        start = filenames[0].split('-')[0].split('.')[-1]
+        end = filenames[-1].split('-')[-1].split('.')[0]
+        da = xr.concat([xr.open_dataset(name)['PRECT'] for name in filenames], dim='time')
+        savedir = '/glade/u/home/hayness/glens/custom'
+        da.to_netcdf('{0}/{1}{2}.PRECT.{3}-{4}.nc'.format(savedir, prefix, n_member, start, end))
+    return True
+
+da, savename = compileFeedback(1)
 da.to_netcdf(savename)
 # compileControl()
