0092ee67064622ddfe04a47f2267ab4a0ca16f1f
HaynesStephens <hfstephens13@gmail.com>
2021-01-20 13:15:46 -0800

    Cropping files to get SoAs and WeAf

--- glens/compileFiles.py
+++ glens/compileFiles.py
@@ -127,5 +127,5 @@ if __name__ == "__main__":
     # for region in regions:
     #     for scen in scens:
     region = 'SoAs'
-    scen   = 'control'
+    scen   = 'rcp'
     getEnsembleMean(scen, region)
