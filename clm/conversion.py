a2e19d8cd8db0ac7fa9ee0c22441b782953c5119
HaynesStephens <hfstephens13@gmail.com>
2021-03-29 16:01:39 -0700

    clm step1 trial

--- clm/conversion.py
+++ clm/conversion.py
@@ -272,5 +272,5 @@ for grainc_name, year in zip(grainc_names, years):
     start_date, end_date = year
     save_name = '{0}.{1}-{2}.nc'.format(grainc_name.replace('GRAINC_TO_FOOD', 'yield_latlon'), start_date, end_date)
     print(grainc, start_date, end_date)
-    # grain4d, land_area = Step1(grainc, start_date, end_date, save_name, save_file=True)
+    grain4d, land_area = Step1(grainc, start_date, end_date, save_name, save_file=True)
 
